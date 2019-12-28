26d8861a474fa270a1881082436a3ca385e682d4
pramodparab <pparab0@gmail.com>
2019-12-28 12:39:23 +0530

    add ckedit and mthjex

---
+++ OnlineExamination/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Student, Question, Exams
+
+admin.site.site_header = "Content Admin"
+
+
+class QuestionModelAdmin(admin.ModelAdmin):
+    list_display = ["question", "exam_name", "marks","option1","option2","option3","option4"]
+
+    class Meta:
+        model = Question
+      
+
+
+
+admin.site.register(Question, QuestionModelAdmin)
+
+admin.site.register(Student)
+
+
+class ExamModelAdmin(admin.ModelAdmin):
+    list_display = ["exam_name", "no_of_ques", "total_marks", "time_duration"]
+
+
+admin.site.register(Exams, ExamModelAdmin)
+
+
+
